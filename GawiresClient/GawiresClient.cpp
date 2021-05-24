7b0eed04970c98864dd631d2309fdd0a3cd402d0
Vincent Roy <vincent.roy@deuzsparktechnologies.com>
2021-05-24 00:05:08 -0400

    modified git ignore and generated default cmake project

---
+++ GawiresClient/GawiresClient.cpp
@@ -0,0 +1,12 @@
+﻿// GawiresClient.cpp : définit le point d'entrée de l'application.
+//
+
+#include "GawiresClient.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
