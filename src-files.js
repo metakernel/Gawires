1f03d15e98075e42090d1841404f09197a4a113f
metakernel <metakernel@outlook.com>
2024-08-19 01:21:29 +0000

    Deploying to gh-pages from @ metakernel/Gawires@6c30f4b645b308fda3e551e17231b65842bfd8b7 🚀

--- src-files.js
+++ src-files.js
@@ -1,5 +1,5 @@
 var srcIndex = new Map(JSON.parse('[\
-["gaw",["",[],["gaw.rs"]]],\
+["gaw",["",[],["main.rs"]]],\
 ["gawires",["",[["cli",[],["commands.rs","handling.rs"]]],["cli.rs","lib.rs"]]]\
 ]'));
 createSrcSidebar();
