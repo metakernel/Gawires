876a3328c5be1c1d4040971ccb37595a4b1e17ff
metakernel <metakernel@outlook.com>
2024-11-17 04:15:21 +0000

    Deploying to gh-pages from @ metakernel/Gawires@5f833327e4487c56aae7a93cbe10759158b13c25 🚀

--- src-files.js
+++ src-files.js
@@ -1,5 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["gaw",["",[],["main.rs"]]],\
-["gawires",["",[["cli",[],["commands.rs","handling.rs"]]],["cli.rs","lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["gaw",["",[],["main.rs"]]],["gawires",["",[["cli",[],["commands.rs","handling.rs"]]],["cli.rs","lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[27,80]}
--- trait.impl/clap_builder/derive/trait.Subcommand.js
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"gawires":[["impl Subcommand for <a class=\"enum\" href=\"gawires/cli/commands/enum.Commands.html\" title=\"enum gawires::cli::commands::Commands\">Commands</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["gawires",[["impl Subcommand for <a class=\"enum\" href=\"gawires/cli/commands/enum.Commands.html\" title=\"enum gawires::cli::commands::Commands\">Commands</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[165]}
